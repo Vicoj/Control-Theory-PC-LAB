7bf3fb477b71225d4df89d4da30f6f5978b89767
Vicoj <vico.jans@gmail.com>
2022-04-29 15:51:32 +0200

    .

--- package_Advanced.py
+++ package_Advanced.py
@@ -37,7 +37,8 @@ def LeadLag_RT(MV,Kp,TLead,TLag,Ts,PV,PVInit=0,method='EDB'):
             if method == 'EBD':
                 PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+TLead/Ts)*MV[-1]- (TLead/Ts)*MV[-2]))
             elif method == 'EFD':
-                PV.append((1-K)*PV[-1] + K*Kp((TLead/Ts)*MV[-1])+(1-TLead/Ts)*MV[-2])
+                #PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+TLead/Ts)*MV[-1]- (TLead/Ts)*MV[-2]))
+                PV.append((1-K)*PV[-1] + (K*Kp)*((TLead/Ts)*MV[-1] +(1-(TLead/Ts))*MV[-2]))
             elif method == 'TRAP':
                 "PV.append((1/(2*T+Ts))*((2*T-Ts)*PV[-1] + Kp*Ts*(MV[-1] + MV[-2])))"            
             else:
@@ -45,40 +46,30 @@ def LeadLag_RT(MV,Kp,TLead,TLag,Ts,PV,PVInit=0,method='EDB'):
     else:
         PV.append(Kp*MV[-1])
 
-def PID_RT(MV,Kp,TLead,TLag,Ts,PV,PVInit=0,method='EDB'):
+def PID_RT(SP, PV, Man, MVMan, MVFF, Kc, Ti, Td, alpha, Ts, MVMin, MVMax, MV, MVp, MVi, MVd, E, ManFF=False, PVInit=0, method='EBD-EBD'):
+    if(len(PV)==0):
+        E.append(SP[-1]-PVInit)
+    else :
+        E.append(SP[-1]-PV[-1])
     
-    """
-    The function "LeadLag_RT" needs to be included in a "for or while loop".
+    MVp.append(Kc*E[-1])
     
-    :MV: input vector
-    :Kp: process gain
-    :T: lag time constant [s]
-    :Ts: sampling period [s]
-    :PV: output vector
-    :PVInit: (optional: default value is 0)
-    :method: discretisation method (optional: default value is 'EBD')
-        EBD: Euler Backward difference
-        EFD: Euler Forward difference
-        TRAP: Trapezoïdal method
+    if(len(MVi)>0):
+        MVi.append(MVi[-1]+(Kc*Ts*E[-1])/Ti)
+    else :
+        MVi.append(0)
     
-    The function "FO_RT" appends a value to the output vector "PV".
-    The appended value is obtained from a recurrent equation that depends on the discretisation method.
-    """    
-    
-    if (T != 0):
-        K = Ts/T
-        if len(PV) == 0:
-            PV.append(PVInit)
-        else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
-            if method == 'EBD':
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
-            elif method == 'EFD':
-                PV.append((1-K)*PV[-1] + K*Kp*MV[-2])
-            elif method == 'TRAP':
-                PV.append((1/(2*T+Ts))*((2*T-Ts)*PV[-1] + Kp*Ts*(MV[-1] + MV[-2])))            
-            else:
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
-    else:
-        PV.append(Kp*MV[-1])
+    Tfd = alpha*Td
+    if(Td>0):
+        if(len(MVd)!=0):
+            if(len(E)==1):
+                MVd.append((Tfd/(Tfd+Ts))*MVd[-1] + ((Kc*Td)/(Tfd+Ts))*(E[-1]))
 
-#-----------------------------------
+            else:
+                MVd.append(( Tfd / (Tfd+Ts) )*MVd[-1] + ( (Kc*Td) / (Tfd+Ts) ) *(E[-1]-E[-2]))
+        else : MVd.append(0)
+  
+
+    MV.append(MVp[-1]+MVi[-1]+MVd[-1])
+    
+    return None
